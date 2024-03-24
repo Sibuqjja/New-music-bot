1b6e4a4722e3513924354b02bc705ad23bfdb00d
Sibuqjja <157947714+Sibuqjja@users.noreply.github.com>
2024-03-25 01:04:17 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  token: process.env.TOKEN || "MTIyMTUzNjI3Mzk0NTIwMjc3OA.GzJmRu.2Q1kEZrqdWNdjupBjlCYgdcrfu2idT8z_d6CXM", // your bot token
+  token: process.env.TOKEN || "", // your bot token
   prefix: process.env.PREFIX || "*", // bot prefix
   ownerID: process.env.OWNERID || ["1187483815334133820"], //your discord id
   mongourl:
